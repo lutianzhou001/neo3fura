83288b5ada61d99639062201c12f2e67c2924cbf
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-07-22 18:37:34 +0800

    add GetCommittee

---
+++ biz/api/src.GetCommittee.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"neo3fura/lib/type/h160"
+	"neo3fura/var/stderr"
+)
+
+func (me *T) GetCommittee(args struct {
+	VoterAddress h160.T
+	Filter       map[string]interface{}
+	Limit        int64
+	Skip         int64
+}, ret *json.RawMessage) error {
+	if args.VoterAddress.Valid() == false {
+		return stderr.ErrInvalidArgs
+	}
+	r1, count, err := me.Client.QueryAll(struct {
+		Collection string
+		Index      string
+		Sort       bson.M
+		Filter     bson.M
+		Query      []string
+		Limit      int64
+		Skip       int64
+	}{
+		Collection: "Candidate",
+		Index:      "GetCommittee",
+		Sort:       bson.M{},
+		Filter:     bson.M{"isCommittee": true},
+		Query:      []string{},
+		Limit:      args.Limit,
+		Skip:       args.Skip,
+	}, ret)
+	if err != nil {
+		return err
+	}
+	r2, err := me.FilterArrayAndAppendCount(r1, count, args.Filter)
+	if err != nil {
+		return err
+	}
+	r, err := json.Marshal(r2)
+	if err != nil {
+		return err
+	}
+	*ret = json.RawMessage(r)
+	return nil
+}
