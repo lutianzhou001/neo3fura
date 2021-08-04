6d47d8132be01ae970ef395f700e9b226d40a1c8
WSbaikaishui <46080358+WSbaikaishui@users.noreply.github.com>
2021-08-04 18:32:45 +0800

    neo3fura to neofura

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo shut down existed docker service
 docker-compose down
 echo remove image of neo3fura_web
-docker rmi neo3fura_web
+docker rmi neofura_web
 echo restart docker service
 docker-compose up -d
 
