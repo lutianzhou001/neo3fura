fe0b5dd5c1acdeae897a1d6028f74a32ac1a483b
陆天洲 <lutianzhou@lutianzhoudeMacBook-Pro.local>
2021-08-15 17:07:34 +0800

    force delete all images

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo remove images
 docker kill $(docker ps -q)
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker rmi $(docker images -q) -f 
 echo restart docker service
 docker-compose up -d
 
