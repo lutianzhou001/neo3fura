70e2f53684a08343bb52939dbf3ecffd4e130828
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-08-17 14:41:38 +0800

    fixed

--- neo3fura_http/biz/api/src.GetContractList.go
+++ neo3fura_http/biz/api/src.GetContractList.go
@@ -7,15 +7,11 @@ import (
 
 func (me *T) GetContractList(args struct {
 	Filter map[string]interface{}
-	Limit  int64
-	Skip   int64
+	Limit        int64
+	Skip         int64
 }, ret *json.RawMessage) error {
 
-	if args.Limit == 0 {
-		args.Limit = 500
-	}
-
-	var r1, err = me.Client.QueryAggregate(
+	var r1, err =me.Client.QueryAggregate(
 		struct {
 			Collection string
 			Index      string
@@ -28,40 +24,68 @@ func (me *T) GetContractList(args struct {
 			Index:      "GetContractList",
 			Sort:       bson.M{},
 			Filter:     bson.M{},
-			Pipeline: []bson.M{
-				bson.M{"$skip": args.Skip},
-				bson.M{"$limit": args.Limit},
+			Pipeline:  []bson.M{
+				bson.M{"$group":
+				bson.M{ "_id":"$hash",
+					"hash": bson.M{"$last":"$hash"},
+					"updatecounter":bson.M{"$last":"$updatecounter"},
+					"createtime":bson.M{ "$last":"$createtime"},
+					"name":bson.M{"$last":"$name"},
+					"id":bson.M{"$last":"$id"},
+					"createTxid":bson.M{"$last":"$createTxid"},
+				},
+				},
+				bson.M{"$sort":bson.M{"id":1}},
+				bson.M{"$skip":args.Skip},
+				bson.M{"$limit":args.Limit},
 				bson.M{"$lookup": bson.M{
-					"from":         "Transaction",
-					"localField":   "createTxid",
+					"from": "Transaction",
+					"localField": "createTxid",
 					"foreignField": "hash",
-					"as":           "Transaction"}},
-				bson.M{"$project": bson.M{"_id": 0, "Transaction.sender": 1, "hash": 1, "createtime": 1, "name": 1, "id": 1}}},
+					"as": "Transaction"}},
+				bson.M{"$project":
+				bson.M{"_id":0,"Transaction.sender":1,"hash":1,"createtime":1,"name":1,"id":1,"updatecounter": 1}}},
 			Query: []string{},
 		}, ret)
 	if err != nil {
 		return err
 	}
-	r3, err := me.Client.QueryDocument(
+	r2, err :=me.Client.QueryAggregate(
 		struct {
 			Collection string
 			Index      string
 			Sort       bson.M
 			Filter     bson.M
-		}{Collection: "Contract",
-			Index:  "someIndex",
-			Sort:   bson.M{},
-			Filter: bson.M{},
+			Pipeline   []bson.M
+			Query      []string
+		}{
+			Collection: "Contract",
+			Index:      "GetContractList",
+			Sort:       bson.M{},
+			Filter:     bson.M{},
+			Pipeline:  []bson.M{
+				bson.M{"$group":
+				bson.M{ "_id":"$hash"},
+				},
+				bson.M{"$count":"total counts"},
+			},
+			Query: []string{},
 		}, ret)
 	if err != nil {
 		return err
 	}
+	var count  interface{}
+	if len(r2) != 0 {
+		count = r2[0]["total counts"]
+	}else{
+		count = 0
+	}
 	//r1 = append(r1, r3)
-	r2, err := me.FilterAggragateAndAppendCount(r1, r3["total counts"], args.Filter)
+	r3, err := me.FilterAggragateAndAppendCount(r1, count, args.Filter)
 	if err != nil {
 		return err
 	}
-	r, err := json.Marshal(r2)
+	r, err := json.Marshal(r3)
 	if err != nil {
 		return err
 	}
--- neo3fura_http/biz/api/src.GetNep11TransferByTransactionHash.go
+++ neo3fura_http/biz/api/src.GetNep11TransferByTransactionHash.go
@@ -2,67 +2,76 @@ package api
 
 import (
 	"encoding/json"
-	"neo3fura_http/lib/type/h256"
-	"neo3fura_http/var/stderr"
+	"neo3fura/lib/type/h256"
+	"neo3fura/var/stderr"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (me *T) GetNep11TransferByTransactionHash(args struct {
 	TransactionHash h256.T
+	Limit           int64
+	Skip            int64
 	Filter          map[string]interface{}
+
 }, ret *json.RawMessage) error {
 	if args.TransactionHash.Valid() == false {
 		return stderr.ErrInvalidArgs
 	}
-	r1, err := me.Client.QueryOne(struct {
+	r1, count,err := me.Client.QueryAll(struct {
 		Collection string
 		Index      string
 		Sort       bson.M
 		Filter     bson.M
 		Query      []string
+		Limit      int64
+		Skip       int64
 	}{
 		Collection: "Nep11TransferNotification",
 		Index:      "GetNep11TransferByTransactionHash",
 		Sort:       bson.M{},
 		Filter:     bson.M{"txid": args.TransactionHash.Val()},
 		Query:      []string{},
+		Limit:      args.Limit,
+		Skip:       args.Skip,
 	}, ret)
 	if err != nil {
 		return err
 	}
 
-	r, err := me.Client.QueryOne(struct {
-		Collection string
-		Index      string
-		Sort       bson.M
-		Filter     bson.M
-		Query      []string
-	}{
-		Collection: "Asset",
-		Index:      "GetNep11TransferByTransactionHash",
-		Sort:       bson.M{},
-		Filter:     bson.M{"hash": r1["contract"]},
-		Query:      []string{"tokenname"},
-	}, ret)
-	if err == nil {
-		r1["tokenname"] = r["tokenname"]
-		r1["decimals"] = r["decimals"]
-	} else if err.Error() == "NOT FOUND" {
-		r1["tokenname"] = ""
-		r1["decimals"] = ""
-	} else {
-		return err
-	}
+	for _, item := range r1 {
+		r, err := me.Client.QueryOne(struct {
+			Collection string
+			Index      string
+			Sort       bson.M
+			Filter     bson.M
+			Query      []string
+		}{
+			Collection: "Asset",
+			Index:      "GetNep11TransferByTransactionHash",
+			Sort:       bson.M{},
+			Filter:     bson.M{"hash": item["contract"]},
+			Query:      []string{"tokenname","decimals"},
+		}, ret)
+		if err == nil {
+			item["tokenname"] = r["tokenname"]
+			item["decimals"] = r["decimals"]
 
-	r1, err = me.Filter(r1, args.Filter)
+		} else if err.Error() == "NOT FOUND" {
+			item["tokenname"] = ""
+			item["decimals"] = ""
+		} else {
+			return err
+		}
+	}
+	r2, err := me.FilterArrayAndAppendCount(r1, count, args.Filter)
 	if err != nil {
 		return err
 	}
-	r2, err := json.Marshal(r1)
+	r, err := json.Marshal(r2)
 	if err != nil {
 		return err
 	}
-	*ret = json.RawMessage(r2)
+	*ret = json.RawMessage(r)
 	return nil
 }
