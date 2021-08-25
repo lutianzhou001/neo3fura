5671037e8250d35d4e1817f25af17023b7779d2d
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-08-25 18:41:56 +0800

    fixed

--- neo3fura_http/config/config.go
+++ neo3fura_http/config/config.go
@@ -9,7 +9,7 @@ var Apis = []string{
 	"GetAssetInfoByContractHash",
 	"GetAssetInfosByName",
 	"GetAddressList",
-	"GetAssetsHeldByAddress",
+	"GetAssetsHeldByContractHashAddress",
 	"GetApplicationLogByBlockHash",
 	"GetApplicationLogByTransactionHash",
 	"GetApplicationLogsByTransactionHash",
