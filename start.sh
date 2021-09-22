de3fb8f4d3db50505710dded161c7278cda12c82
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-09-22 17:08:17 +0800

    remove some of the service not restart in execting bash ./start.sh

--- start.sh
+++ start.sh
@@ -6,16 +6,9 @@ then
     export RUNTIME="test"
     docker stop service_ws_test
     docker stop service_http_test
-    docker stop cache_test
-    docker stop mongo_test
-    docker stop prometheus_test
-    docker stop grafana_test
 
     docker container rm service_ws_test
     docker container rm service_http_test
-    docker container rm cache_test
-    docker container rm prometheus_test
-    docker container rm grafana_test
 
     docker rmi test_neofura_http -f
     docker rmi test_neofura_ws -f
@@ -27,16 +20,9 @@ then
     export RUNTIME="staging"
     docker stop service_ws_staging
     docker stop service_http_staging
-    docker stop cache_staging
-    docker stop mongo_staging
-    docker stop prometheus_staging
-    docker stop grafana_staging
 
     docker container rm service_ws_staging
     docker container rm service_http_staging
-    docker container rm cache_staging
-    docker container rm prometheus_staging
-    docker container rm grafana_staging
 
     docker rmi staging_neofura_http -f
     docker rmi staging_neofura_ws -f
