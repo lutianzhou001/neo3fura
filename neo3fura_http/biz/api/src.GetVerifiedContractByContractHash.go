992b1d29abf9787a0e45956ac1045c03b0efc526
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-10-26 11:42:11 +0800

    add updatecounter into GetVerifiedContractByContractHash

--- neo3fura_http/biz/api/src.GetVerifiedContractByContractHash.go
+++ neo3fura_http/biz/api/src.GetVerifiedContractByContractHash.go
@@ -4,16 +4,22 @@ import (
 	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
 	"neo3fura_http/lib/type/h160"
+	"neo3fura_http/lib/type/uintval"
 	"neo3fura_http/var/stderr"
 )
 
 func (me *T) GetVerifiedContractByContractHash(args struct {
-	Filter       map[string]interface{}
-	ContractHash h160.T
+	ContractHash  h160.T
+	UpdateCounter  uintval.T
+	Filter  map[string]interface{}
 }, ret *json.RawMessage) error {
-	if args.ContractHash.Valid() != true {
+	if args.ContractHash.Valid() == false {
 		return stderr.ErrInvalidArgs
 	}
+	if args.UpdateCounter.Valid() == false {
+		return stderr.ErrInvalidArgs
+	}
+
 	r1, err := me.Client.QueryOne(struct {
 		Collection string
 		Index      string
@@ -24,16 +30,22 @@ func (me *T) GetVerifiedContractByContractHash(args struct {
 		Collection: "VerifyContract",
 		Index:      "GetVerifiedContractByContractHash",
 		Sort:       bson.M{},
-		Filter:     bson.M{"hash": args.ContractHash.Val()},
+		Filter:     bson.M{"hash": args.ContractHash,"updatecounter":args.UpdateCounter},
 		Query:      []string{},
 	}, ret)
+
 	if err != nil {
 		return err
 	}
-	r2, err := json.Marshal(r1)
+
+	r2, err := me.Filter(r1, args.Filter)
 	if err != nil {
 		return err
 	}
-	*ret = json.RawMessage(r2)
+	r, err := json.Marshal(r2)
+	if err != nil {
+		return err
+	}
+	*ret = json.RawMessage(r)
 	return nil
 }
