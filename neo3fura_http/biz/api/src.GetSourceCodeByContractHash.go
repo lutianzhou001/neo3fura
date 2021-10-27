d7aa207e792e1e737889ba5be8eab5d3c4a9d88c
yljjgroup <tianzhou.lu@jiajiagroup.net>
2021-10-27 17:29:10 +0800

    add api GetSourceCodeByContractHash.go

---
+++ neo3fura_http/biz/api/src.GetSourceCodeByContractHash.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"neo3fura_http/lib/type/h160"
+	"neo3fura_http/lib/type/uintval"
+	"neo3fura_http/var/stderr"
+)
+
+func (me *T) GetSourceCodeByContractHash(args struct {
+	Limit  int64
+	Skip   int64
+	ContractHash h160.T
+	UpdateCounter uintval.T
+	Filter map[string]interface{}
+}, ret *json.RawMessage) error {
+	if args.ContractHash.Valid() == false {
+		return stderr.ErrInvalidArgs
+	}
+	if args.UpdateCounter.Valid() == false {
+		return stderr.ErrInvalidArgs
+	}
+	if args.Limit <= 0 {
+		args.Limit = 20
+	}
+	r1, count, err := me.Client.QueryAll(struct {
+		Collection string
+		Index      string
+		Sort       bson.M
+		Filter     bson.M
+		Query      []string
+		Limit      int64
+		Skip       int64
+	}{
+		Collection: "ContractSourceCode",
+		Index:      "someIndex",
+		Sort:       bson.M{},
+		Filter:     bson.M{"hash":args.ContractHash,"updatecounter":args.UpdateCounter},
+		Query:      []string{},
+		Limit:      args.Limit,
+		Skip:       args.Skip,
+	}, ret)
+	if err != nil {
+		return err
+	}
+	r2, err := me.FilterArrayAndAppendCount(r1, count, args.Filter)
+	if err != nil {
+		return err
+	}
+	r, err := json.Marshal(r2)
+	if err != nil {
+		return err
+	}
+	*ret = json.RawMessage(r)
+	return nil
+}
