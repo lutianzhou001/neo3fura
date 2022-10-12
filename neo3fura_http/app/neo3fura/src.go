4d323c2e0244450a66c29083ac70c294c184e249
Tobeyw <tobey1024@126.com>
2022-10-12 22:06:59 +0800

    fix properties

--- neo3fura_http/app/neo3fura/src.go
+++ neo3fura_http/app/neo3fura/src.go
@@ -183,8 +183,7 @@ func main() {
 			go j.GetMarketSupply()
 			go j.GetMarketTxAmount()
 			go j.GetMarketOwnerCount()
-			//go j.GetNFTFloorPrice()
-			go j.GetNFTIndex()
+			go j.GetNFTFloorPrice()
 		})
 		if err != nil {
 			log2.Fatal("add job function error:%s", err)
